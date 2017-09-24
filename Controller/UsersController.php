8c2ae37211f1ab6619d2d4d7f6812a98a924ff83
thorus <thorus90@googlemail.com>
2017-09-24 17:41:47 +0200

    updated to cakephp 3.5.2

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -11,16 +11,16 @@ class UsersController extends AppController
 
     public $helpers = [
         'Html' => [
-            'className' => 'Bootstrap3.BootstrapHtml'
+            'className' => 'Bootstrap.BootstrapHtml'
         ],
         'Form' => [
-            'className' => 'Bootstrap3.BootstrapForm'
+            'className' => 'Bootstrap.BootstrapForm'
         ],
         'Paginator' => [
-            'className' => 'Bootstrap3.BootstrapPaginator'
+            'className' => 'Bootstrap.BootstrapPaginator'
         ],
         'Modal' => [
-            'className' => 'Bootstrap3.BootstrapModal'
+            'className' => 'Bootstrap.BootstrapModal'
         ]
     ];
      
